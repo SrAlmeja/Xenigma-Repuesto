7e0bce8dd6b7840ba2d2aacf54bd980e4064aef9
Angel Legorreta <cdmx1895@amerike.edu>
2023-07-12 00:56:38 -0500

    Agregando Assets

---
+++ Xenigma Juegos/Assets/Code/Memoria de la Historia/MemoryGameController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MemoryGameController : MonoBehaviour
+{
+   [SerializeField] private Sprite bgImage;
+   
+   public List<Button> btns = new List<Button>();
+
+   private void Start()
+   {
+      GetButtons();
+   }
+
+   void GetButtons()
+   {
+      GameObject[] objects = GameObject.FindGameObjectsWithTag("PuzzleMemoryButton");
+
+      for (int i = 0; i < objects.Length; i++)
+      {
+         btns.Add(objects[i].GetComponent<Button>());
+         btns[i].image.sprite = bgImage;
+      }
+   }
+}
